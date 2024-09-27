566f20547117accc661ee906d4c05865589a69e8
Niels Weistra <n.weistra@itlusions.nl>
2024-09-27 23:36:33 +0200

    Fix import pydantic_settings

--- cveimporter/base/config.py
+++ cveimporter/base/config.py
@@ -1,4 +1,4 @@
-from pydantic-settings import BaseSettings, Field
+from pydantic_settings import BaseSettings, Field
 
 class Settings(BaseSettings):
     nvd_api_url: str = Field(..., env="NVD_API_URL")
